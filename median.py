a79124f7a33c460739ca3c9a246f61654b126c2b
Susie James <susetta.james@kcl.ac.uk>
2022-10-02 19:47:56 +0100

    median sorted

--- median.py
+++ median.py
@@ -10,3 +10,13 @@ while True:
     else:
         break
 print(numbers)
+
+sum = len(numbers)
+median = 0
+if(sum%2) == 0:
+    num1 = numbers[int((sum/2)-1)]
+    num2 = numbers[int((sum/2))]
+    count = (num1 + num2)/2
+    print(count)
+else:
+    print(numbers[int(sum/2)])
